aa7fa6e8c613650cf4bf6f30618e946ec3f04249
DaliVidic <daliborka_vg@hotmail.com>
2025-02-22 20:12:23 +0000

    Variables - section 2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert('This works')
+const defaultResult = 0;
+
+let result = defaultResult;
+
+result = (result + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(result,calculationDescription);
