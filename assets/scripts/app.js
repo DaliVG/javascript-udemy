3ea2a8a45579971d3d35d61e3adbecfcc769749f
DaliVidic <daliborka_vg@hotmail.com>
2025-02-22 20:33:01 +0000

    Adding a function - section 2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,14 @@ const defaultResult = 0;
 
 let result = defaultResult;
 
+
+function add(number1, number2){
+    const result = number1 + number2;
+    alert('The result is '+ result)
+}
+
+add(2, 3);
+add(1, 2);
 result = (result + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
