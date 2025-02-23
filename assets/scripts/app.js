70538fa7bd6dd84f1ebe8534e5a7d3be66d3dfc8
DaliVidic <daliborka_vg@hotmail.com>
2025-02-23 08:35:58 +0000

    All first calculator - section 2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,43 @@
 const defaultResult = 0;
 
-let result = defaultResult;
+let currentResult = defaultResult;
+
+function getUserInput(){
+  return parseInt(usrInput.value);
+}
+
+function createAndWriteLog(operator, resultBeforeCacl, calcNumber){
+  const calculationDescription = `${resultBeforeCacl} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calculationDescription);
+}
 
 function add() {
-  result = result + parseInt(userInput.value);
-  outputResult(result, '');
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteLog('+', initialResult, enteredNumber);
+}
+
+function substrac() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteLog('-', initialResult, enteredNumber);
+}
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteLog('*', initialResult, enteredNumber);
+}
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteLog('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substrac);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
