0004ca1c66b0c4a725cb8717014e1fa2c5fc328a
Daliborka Vidic Gutierrez <e_dvidic@mercadona.com>
2025-05-11 18:38:26 +0200

    Add to cart method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,35 @@ class Product {
     this.description = desc;
     this.price = price;
   }
+}
 
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
+  render() {
+    const cartEl = document.createElement('section');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = 'cart';
+    this.totalOutput = cartEl.querySelector('h2');
+    return cartEl;
+  }
 }
 
 class ProductItem {
   constructor(product) {
     this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
   }
 
   render() {
@@ -28,12 +51,14 @@ class ProductItem {
           </div>
         </div>
       `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
     return prodEl;
   }
 }
 
-const productList = {
-  products: [
+class ProductList {
+  products = [
     new Product(
       'A Pillow',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDddlMzV5Cl5V-34NxWaf-Iy_7iFfgwDuJVQ&s',
@@ -46,30 +71,49 @@ const productList = {
       'A carpet which you might like - or not.',
       89.99
     )
-  ],
-  
+  ];
+
+  constructor() {}
+
   render() {
-    const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}" >
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList;
   }
-};
+}
 
-productList.render();
+class Shop {
+  
+  render() {
+    const renderHook = document.getElementById('app');
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
